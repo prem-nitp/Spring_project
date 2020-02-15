d3a2e1ad01955089a8888d81f51cfa5f330e9456
prem_kumar <prem190996@gmail.com>
2020-02-15 15:26:38 +0530

    Add Textfield and label

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -1,16 +1,34 @@
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
+	
+	private JLabel nameLabel;
+	private JLabel occupationLabel;
+	private JTextField nameTextField;
+	private JTextField occupationField;
+	private JButton okBtn;
+	
 	public FormPanel()
 	{
 		Dimension dim = getPreferredSize();
 		dim.width=250;
 		setPreferredSize(dim);
 		
+		nameLabel = new JLabel("Name: ");
+		occupationLabel = new JLabel("Occupation: ");
+		nameTextField = new JTextField();
+		occupationField = new JTextField();
+		okBtn = new JButton("OK");
+		add(nameLabel);
+		add(occupationLabel);
+		//add(name)
 		Border innerBorder = BorderFactory.createTitledBorder("Add person");
 		Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
