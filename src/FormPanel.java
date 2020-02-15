b4c070e2358362721fb43f09e58850586f968c2a
prem_kumar <prem190996@gmail.com>
2020-02-15 15:53:13 +0530

    Added JTextField and JLabel

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -1,15 +1,31 @@
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
+	
+	private JLabel nameLabel;
+	private JLabel occupationLabel;
+	private JTextField nameTextField;
+	private JTextField occupationField;
+	private JButton okBtn;
+	
 	public FormPanel()
 	{
 		Dimension dim = getPreferredSize();
 		dim.width=250;
 		setPreferredSize(dim);
+		
+		nameLabel = new JLabel("Name: ");
+		occupationLabel = new JLabel("Occupation: ");
+		nameTextField = new JTextField();
+		occupationField = new JTextField();
+		okBtn = new JButton("OK");
 		
 		Border innerBorder = BorderFactory.createTitledBorder("Add person");
 		Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
